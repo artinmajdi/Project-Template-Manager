54131a399510c948c11a059828cb4a25a4599fe0
Artin Majdi <msm2024@gmail.com>
2025-04-05 16:36:30 -0700

    Remove all files related to the pythonic_template project, including source code, configuration, and documentation. Update VS Code extension version to 1.0.0.

--- pythonic_template/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Test package for the project template.
-
-This package contains all test modules and fixtures for testing the project.
-"""
-
-# Import any test fixtures or utility functions that should be available to all tests
